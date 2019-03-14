bafc8f49b2055e310fe7714dd24320cdb0bc93b1
Azman <azman95ict@gmail.com>
2019-03-15 00:07:53 +0800

    Updates

---
+++ precache-manifest.2c96bf30781f5951831301f4265ef915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pinvent/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3227644514797faa2f9f",
+    "url": "/pinvent/static/js/runtime~main.fcc29352.js"
+  },
+  {
+    "revision": "f5e313e6580db0168ddf",
+    "url": "/pinvent/static/js/main.bbce50c8.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/pinvent/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "f5e313e6580db0168ddf",
+    "url": "/pinvent/static/css/main.f3aa1d0f.chunk.css"
+  },
+  {
+    "revision": "1274ff93717aa3d2e91e6b778f48656e",
+    "url": "/pinvent/index.html"
+  }
+];
